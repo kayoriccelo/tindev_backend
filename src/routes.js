9c386473a600b6ecccd390ae47eef3d459a0a94f
Kayo Riccelo <kayo.desenvolvimento@gmail.com>
2019-08-09 19:28:10 -0300

    preparando rotas, server e criando model Dev.

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,12 @@ const routes = express.Router();
 
 routes.get('/', (req, res) => {
     return res.json({message: `Olá ${req.query.name}`});
+})
+
+routes.post('/devs', (req, res) => {
+    console.log(req.body);
+    
+    return res.json({ok: true});
 });
 
 module.exports = routes;
